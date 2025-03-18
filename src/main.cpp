8296ef73cffe46a7ace0ae453ae4bfa3040c441e
Maymeric <aymeric.marec@gmail.com>
2025-03-18 12:34:25 +0100

    wifi connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
+#define LED1 2
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* password = "TartelettE";
+const char* ssid = "Moi";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting");
+  
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
